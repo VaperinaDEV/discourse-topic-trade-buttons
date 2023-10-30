1db6688dc76e1cd0499b7effb573811f659a1da3
Don <71207900+VaperinaDEV@users.noreply.github.com>
2023-10-30 14:54:38 +0100

    Update extend-category-for-topic-trade-buttons.js

--- assets/javascripts/discourse/pre-initializers/extend-category-for-topic-trade-buttons.js
+++ assets/javascripts/discourse/pre-initializers/extend-category-for-topic-trade-buttons.js
@@ -1,13 +1,11 @@
-import property from "discourse-common/utils/decorators";
 import Category from "discourse/models/category";
+import property from "discourse-common/utils/decorators";
 
 export default {
   name: "extend-category-for-topic-trade-buttons",
   before: "inject-discourse-objects",
   initialize() {
-
     Category.reopen({
-
       @property("custom_fields.enable_sold_button")
       enable_sold_button: {
         get(enableField) {
@@ -17,7 +15,7 @@ export default {
           value = value ? "true" : "false";
           this.set("custom_fields.enable_sold_button", value);
           return value;
-        }
+        },
       },
 
       @property("custom_fields.enable_purchased_button")
@@ -29,7 +27,7 @@ export default {
           value = value ? "true" : "false";
           this.set("custom_fields.enable_purchased_button", value);
           return value;
-        }
+        },
       },
 
       @property("custom_fields.enable_exchanged_button")
@@ -41,7 +39,7 @@ export default {
           value = value ? "true" : "false";
           this.set("custom_fields.enable_exchanged_button", value);
           return value;
-        }
+        },
       },
 
       @property("custom_fields.enable_cancelled_button")
@@ -53,9 +51,8 @@ export default {
           value = value ? "true" : "false";
           this.set("custom_fields.enable_cancelled_button", value);
           return value;
-        }
-      }
-
+        },
+      },
     });
-  }
+  },
 };
