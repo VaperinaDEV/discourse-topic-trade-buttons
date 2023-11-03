a877af85d5ab9b47fd42e636e43fdbfce598d1f1
Don <71207900+VaperinaDEV@users.noreply.github.com>
2023-11-03 04:41:51 +0100

    FIX: Incorrect PLUGIN_NAME

--- plugin.rb
+++ plugin.rb
@@ -5,7 +5,7 @@
 
 enabled_site_setting :topic_trade_buttons_enabled
 
-PLUGIN_NAME ||= "discourse_topic_trade_buttons".freeze
+PLUGIN_NAME ||= "discourse-topic-trade-buttons".freeze
 
 after_initialize do
 
